d462b5378aee263deb9e84a85f76754de4354207
Quaint34 <zandirlearoyd@outlook.com>
2024-06-27 21:10:49 +1000

    Git LFS

--- Game/Assets/Scripts/MoveDiscrete.cs
+++ Game/Assets/Scripts/MoveDiscrete.cs
@@ -13,14 +13,19 @@ public class MoveDiscrete : MonoBehaviour
 	public float distanceX;
 	public float distanceY;
 
+	public AudioClip clip;
+	public AudioSource source;
 
 	private Rigidbody2D rigidBody;
+	private float soundTimer;
 
 
 	// Use this for initialization
 	void Start () 
 	{
 		rigidBody = GetComponent<Rigidbody2D> ();
+		source = GetComponent<AudioSource> ();
+		soundTimer = Random.Range(10f, 15f);
 	}
 	
 	// Update is called once per frame
@@ -30,5 +35,12 @@ public class MoveDiscrete : MonoBehaviour
 		if (Input.GetKeyDown (rightKey))rigidBody.MovePosition(transform.position + new Vector3 (distanceX, 0, 0));
 		if (Input.GetKeyDown (upKey))	rigidBody.MovePosition(transform.position + new Vector3 (0, distanceY, 0));
 		if (Input.GetKeyDown (downKey))	rigidBody.MovePosition(transform.position + new Vector3 (0, -distanceY, 0));
+		
+		while(soundTimer > 0) { soundTimer -= 0.1f; }
+		if (soundTimer <= 0 && !source.isPlaying) 
+		{
+			source.PlayOneShot(clip);
+			soundTimer = Random.Range(10f, 15f);
+		}
 	}
 }
---
+++ Game/Assets/Scripts/SpriteFlip.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlip : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    float xValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        xValue = transform.position.x;
+
+        if(xValue >  0f) { sprite.flipX = true; }
+        else { sprite.flipX = false; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
